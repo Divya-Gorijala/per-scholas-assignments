f55a39426f477dae391c3c8d16ebb82983a96795
Divya <gvdivyasai@gmail.com>
2024-03-27 13:42:36 -0700

    PA 303.9.1 - Practice Assignment - Inheritance and overloading

---
+++ src/main/java/com/perscholas/java_basics/encapsulation/pa_inheritance_and_overloading/MotorCycleSolution.java
@@ -0,0 +1,32 @@
+package com.perscholas.java_basics.encapsulation.pa_inheritance_and_overloading;
+
+import java.util.*;
+import java.io.*;
+
+
+class BiCycle{
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle{
+    String define_me(){
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle(){
+        System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+        String temp=super.define_me(); //Fix this line
+
+        System.out.println("My ancestor is a cycle who is "+ temp );
+    }
+
+}
+class MotorCycleSolution{
+    public static void main(String []args){
+        MotorCycle M=new MotorCycle();
+    }
+}
+
---
+++ src/main/java/com/perscholas/java_basics/encapsulation/practise/GetterSetter.java
@@ -0,0 +1,4 @@
+package com.perscholas.java_basics.encapsulation.practise;
+
+public class GetterSetter {
+}
